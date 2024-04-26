4f30ed99c2f47977809b9238205d272fe600a14a
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-04-26 15:38:58 +0600

    [Done] env

--- src/configuration/DevConfig.ts
+++ src/configuration/DevConfig.ts
@@ -2,7 +2,7 @@
 
 export default {
     DOMAIN: process.env.REACT_APP_DEV_DOMAIN,
-    API_BASE_URL: `${process.env.REACT_APP_DEV_API_BASE}/graphql`,
+    API_BASE_URL: `${process.env.REACT_APP_API_BASE}/graphql`,
     // API_BASE_URL: "https://api.piratemobile.gg/graphql",
     GOOGLE_CLIENT_ID:
         "1060585065695-kl1dpg1pv18kq6vvuqsjos19laptjc9g.apps.googleusercontent.com",
