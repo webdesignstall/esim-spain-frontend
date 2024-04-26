2c1e21f790044dcf10eacf4a47f14ced2c46a17e
developeralamgirbd <developer.alamgirbdg@gmail.com>
2024-04-26 11:58:14 +0600

    [Done] domain from env

--- src/configuration/ProConfig.ts
+++ src/configuration/ProConfig.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
-    DOMAIN: "https://www.piratemobile.gg",
-    API_BASE_URL: "https://api.piratemobile.gg/graphql",
+    DOMAIN: process.env.REACT_APP_DOMAIN,
+    API_BASE_URL: `${process.env.REACT_APP_API_BASE}/graphql`,
     GOOGLE_CLIENT_ID:
         process.env.GOOGLE_CLIENT_ID ||
         "596252792983-lkfvcj3ksq85c1h5b8tsgie5lrr7jp4o.apps.googleusercontent.com",
