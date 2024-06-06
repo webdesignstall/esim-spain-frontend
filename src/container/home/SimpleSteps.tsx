c29a6f1cb032b21a3bfa54ad94b54db86c913ec8
Sahidul Islam Jony <jonyahmed19@gmail.com>
2024-06-06 21:13:13 +0600

    fixed simple steps

--- src/container/home/SimpleSteps.tsx
+++ src/container/home/SimpleSteps.tsx
@@ -6,15 +6,15 @@ import VerticalTimelineMobile from "./VerticalTimelineMobile";
 
 const SimpleSteps = () => {
   return (
-    <div className="bg-[#1F1B17] py-[120px] simple-section">
+    <div className="bg-[#1F1B17] py-[60px] md:py-[120px] simple-section">
       <div className="custom-continer-inner flex flex-col">
-        <div className="text-center mb-14">
+        <div className="text-center mb-8 md:mb-14">
           <h2 className="lg:text-[45px] text-3xl text-[#FFFFFF]">
             3 Simple Steps
           </h2>
-          <p className="text-lg mt-7 text-[#D2D2D2]">To Active Your eSims</p>
+          <p className="text-lg mt-3 md:mt-7 text-[#D2D2D2]">To Active Your eSims</p>
         </div>
-        <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        <div className="flex flex-col-reverse md:flex-row justify-between items-center gap-16  md:gap-0">
           <div className={'simple-bottom-left'}>
             <VerticalUITimeline />
           </div>
